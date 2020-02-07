b59660e95d0a9ec75c82ec34cc906508b47d2213
Paul <planetbutler@gmail.com>
2020-02-07 10:15:39 -0500

    worked on musician select

--- src/views/bookingTool.vue
+++ src/views/bookingTool.vue
@@ -32,36 +32,36 @@
         <h2>Select your Musicians</h2>
         <div>
           <div>Lead Trumpet
-            <select v-model="trumpet" multiple="true">
-              <option v-for="(cat, index) in trumpetPlayers" :value="cat" :key="index">{{ cat.nickName }} {{ cat.last }}</option>
+            <select v-model="trumpet1" @change="filterMusicians">
+              <option v-for="(cat, index) in trumpet1Players" :value="cat" :key="index+1">{{ cat.nickName }} {{ cat.last }}</option>
             </select>
           </div>    
         </div>
         <div>
           <div>Trumpet 2
-            <select v-model="trumpet" multiple="true">
-              <option v-for="(cat, index) in trumpetPlayers" :value="cat" :key="index">{{ cat.nickName }} {{ cat.last }}</option>
+            <select v-model="trumpet2" @change="filterMusicians">
+              <option v-for="(cat, index) in trumpet2Players" :value="cat" :key="index+1">{{ cat.nickName }} {{ cat.last }}</option>
             </select>
           </div>    
         </div>
         <div>
           <div>Trumpet 3
-            <select v-model="trumpet" multiple="true">
-              <option v-for="(cat, index) in trumpetPlayers" :value="cat" :key="index">{{ cat.nickName }} {{ cat.last }}</option>
+            <select v-model="trumpet3" @change="filterMusicians">
+              <option v-for="(cat, index) in trumpet3Players" :value="cat" :key="index+1">{{ cat.nickName }} {{ cat.last }}</option>
             </select>
           </div>    
         </div>
         <div>
           <div>Trumpet 4
-            <select v-model="trumpet" multiple="true">
-              <option v-for="(cat, index) in trumpetPlayers" :value="cat" :key="index">{{ cat.nickName }} {{ cat.last }}</option>
+            <select v-model="trumpet4" @change="filterMusicians">
+              <option v-for="(cat, index) in trumpet4Players" :value="cat" :key="index+1">{{ cat.nickName }} {{ cat.last }}</option>
             </select>
           </div>  
         </div>
         <div>
           <div>Trumpet 5 (jazz chair)
-            <select v-model="trumpet" multiple="true">
-              <option v-for="(cat, index) in trumpetPlayers" :value="cat" :key="index">{{ cat.nickName }} {{ cat.last }}</option>
+            <select v-model="trumpet5" @change="filterMusicians">
+              <option v-for="(cat, index) in trumpet5Players" :value="cat" :key="index+1">{{ cat.nickName }} {{ cat.last }}</option>
             </select>  
           </div>  
         </div>
@@ -83,29 +83,57 @@
         </div>    
         <div>
           <div>Tenor 1
-            <select v-model="tenor"> 
-              <option v-for="(cat, index) in tenorPlayers" :value="cat" :key="index">{{ cat.nickName }} {{ cat.last }}</option>
+            <select v-model="tenor1" @change="filterMusicians"> 
+              <option v-for="(cat, index) in tenor1Players" :value="cat" :key="index+1">{{ cat.nickName }} {{ cat.last }}</option>
             </select>
           </div> 
         </div>
         <div>
           <div>Tenor 2
-            <select v-model="tenor"> 
-              <option v-for="(cat, index) in tenorPlayers" :value="cat" :key="index">{{ cat.nickName }} {{ cat.last }}</option>
+            <select v-model="tenor2" @change="filterMusicians"> 
+              <option v-for="(cat, index) in tenor2Players" :value="cat" :key="index+1">{{ cat.nickName }} {{ cat.last }}</option>
             </select>
           </div>  
         </div>
         <div>
           <div>Bari
             <select v-model="bari"> 
-              <option v-for="(cat, index) in bariPlayers" :value="cat" :key="index">{{ cat.nickName }} {{ cat.last }}</option>
+              <option v-for="(cat, index) in bariPlayers" :value="cat" :key="index+1">{{ cat.nickName }} {{ cat.last }}</option>
             </select>
           </div>  
         </div>
         <div>
-          <div>Bones
-            <select v-model="bone" :close-on-select="true"> 
-              <option v-for="(cat, index) in bonePlayers" :value="cat" :key="index">{{ cat.nickName }} {{ cat.last }}</option>
+          <div>Bone 1
+            <select v-model="bone1" @change="filterMusicians"> 
+              <option v-for="(cat, index) in bone1Players" :value="cat" :key="index+1">{{ cat.nickName }} {{ cat.last }}</option>
+            </select>
+          </div>  
+        </div>
+        <div>
+          <div>Bone 2
+            <select v-model="bone2" @change="filterMusicians"> 
+              <option v-for="(cat, index) in bone2Players" :value="cat" :key="index+1">{{ cat.nickName }} {{ cat.last }}</option>
+            </select>
+          </div>  
+        </div>
+        <div>
+          <div>Bone 3
+            <select v-model="bone3" @change="filterMusicians"> 
+              <option v-for="(cat, index) in bone3Players" :value="cat" :key="index+1">{{ cat.nickName }} {{ cat.last }}</option>
+            </select>
+          </div>  
+        </div>
+        <div>
+          <div>Bone 4
+            <select v-model="bone4" @change="filterMusicians"> 
+              <option v-for="(cat, index) in bone4Players" :value="cat" :key="index+1">{{ cat.nickName }} {{ cat.last }}</option>
+            </select>
+          </div>  
+        </div>
+        <div>
+          <div>Bass Bone
+            <select v-model="bone5" @change="filterMusicians"> 
+              <option v-for="(cat, index) in bone5Players" :value="cat" :key="index+1">{{ cat.nickName }} {{ cat.last }}</option>
             </select>
           </div>  
         </div>
@@ -148,17 +176,26 @@
       <div class="datecol">
         <h2 class="maker">This date's current personnel are:</h2>
         <div class="maker">
-          <div v-show="trumpet.nickName">On Trumpet {{ trumpet.nickName }} {{ trumpet.last }}</div>
-          <div v-show="alto1.first">On Alto Sax {{ alto1.nickName }} {{ alto1.last }}</div>
-          <div v-show="alto2.first">On Alto Sax {{ alto2.nickName }} {{ alto2.last }}</div>
-          <div v-show="tenor.first">On Tenor Sax {{ tenor.nickName }} {{ tenor.last }}</div>
-          <div v-show="bari.first">On Bari Sax {{ bari.nickName }} {{ bari.last }}</div>
-          <div v-show="bone.first">On Trombone {{ bone.nickName }} {{ bone.last }}</div>
-          <div v-show="piano.first">On Piano {{ piano.nickName }} {{ piano.last }}</div>
-          <div v-show="bass.first">On Bass {{ bass.nickName }} {{ bass.last }}</div>
-          <div v-show="drummer.first">On Drums {{ drummer.nickName }} {{ drummer.last }}</div>
-          <div v-show="guitar.first">On Guitar {{ guitar.nickName }} {{ guitar.last }}</div>
-          <div v-show="vocalist.first">On Vocals {{ vocalist.nickName }} {{ vocalist.last }}</div>
+          <div v-show="trumpet1.nickName">On Trumpet {{ trumpet1.nickName }} {{ trumpet1.last }}</div>
+          <div v-show="trumpet2.nickName">On Trumpet {{ trumpet2.nickName }} {{ trumpet2.last }}</div>
+          <div v-show="trumpet3.nickName">On Trumpet {{ trumpet3.nickName }} {{ trumpet3.last }}</div>
+          <div v-show="trumpet4.nickName">On Trumpet {{ trumpet4.nickName }} {{ trumpet4.last }}</div>
+          <div v-show="trumpet5.nickName">On Trumpet {{ trumpet5.nickName }} {{ trumpet5.last }}</div>
+          <div v-show="alto1.nickName">On Alto Sax {{ alto1.nickName }} {{ alto1.last }}</div>
+          <div v-show="alto2.nickName">On Alto Sax {{ alto2.nickName }} {{ alto2.last }}</div>
+          <div v-show="tenor1.nickName">On Tenor Sax {{ tenor1.nickName }} {{ tenor1.last }}</div>
+          <div v-show="tenor2.nickName">On Tenor Sax {{ tenor2.nickName }} {{ tenor2.last }}</div>
+          <div v-show="bari.nickName">On Bari Sax {{ bari.nickName }} {{ bari.last }}</div>
+          <div v-show="bone1.nickName">On Trombone {{ bone1.nickName }} {{ bone1.last }}</div>
+          <div v-show="bone2.nickName">On Trombone {{ bone2.nickName }} {{ bone2.last }}</div>
+          <div v-show="bone3.nickName">On Trombone {{ bone3.nickName }} {{ bone3.last }}</div>
+          <div v-show="bone4.nickName">On Trombone {{ bone4.nickName }} {{ bone4.last }}</div>
+          <div v-show="bone5.nickName">On Trombone {{ bone5.nickName }} {{ bone5.last }}</div>
+          <div v-show="piano.nickName">On Piano {{ piano.nickName }} {{ piano.last }}</div>
+          <div v-show="bass.nickName">On Bass {{ bass.nickName }} {{ bass.last }}</div>
+          <div v-show="drummer.nickName">On Drums {{ drummer.nickName }} {{ drummer.last }}</div>
+          <div v-show="guitar.nickName">On Guitar {{ guitar.nickName }} {{ guitar.last }}</div>
+          <div v-show="vocalist.nickName">On Vocals {{ vocalist.nickName }} {{ vocalist.last }}</div>
           <button @click="addBand">Book the Band!</button>
         </div>
         <div>  
@@ -192,12 +229,21 @@ export default {
   },
   data() {
     return {
-      trumpet: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
+      trumpet1: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
+      trumpet2: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
+      trumpet3: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
+      trumpet4: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
+      trumpet5: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
       alto1: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
       alto2: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
-      tenor: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
+      tenor1: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
+      tenor2: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
       bari: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
-      bone: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
+      bone1: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
+      bone2: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
+      bone3: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
+      bone4: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
+      bone5: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
       piano: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
       bass: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
       drummer: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
@@ -205,12 +251,21 @@ export default {
       vocalist: {first:null, last:null, nickName:null, axe:null, style:null, format:null, dob:null, dod:null},
       style: '',
       format: '',
-      trumpetPlayers:[],
+      trumpet1Players:[],
+      trumpet2Players:[],
+      trumpet3Players:[],
+      trumpet4Players:[],
+      trumpet5Players:[],
       alto1Players:[],
       alto2Players:[],
-      tenorPlayers:[],
+      tenor1Players:[],
+      tenor2Players:[],
       bariPlayers:[],
-      bonePlayers:[],
+      bone1Players:[],
+      bone2Players:[],
+      bone3Players:[],
+      bone4Players:[],
+      bone5Players:[],
       pianoPlayers:[],
       bassPlayers:[],
       drummerPlayers:[],
@@ -245,16 +300,64 @@ export default {
       this.$store.dispatch('addBand', band);
     },
     filterMusicians() {
-      this.trumpetPlayers=this.musicians.filter(item => item.axe==='trumpet');
+      this.trumpet1Players=this.musicians
+        .filter(item => item.axe==='trumpet')
+        .filter(item => item.nickName !== this.trumpet2.nickName 
+        && this.trumpet3.nickName && this.trumpet4.nickName 
+        && this.trumpet5.nickName);
+      this.trumpet2Players=this.musicians
+      .filter(item => item.axe==='trumpet')
+        .filter(item => item.nickName !== this.trumpet1.nickName 
+        && this.trumpet3.nickName && this.trumpet4.nickName 
+        && this.trumpet5.nickName);
+      this.trumpet3Players=this.musicians
+      .filter(item => item.axe==='trumpet')
+        .filter(item => item.nickName !== this.trumpet1.nickName 
+        && this.trumpet2.nickName && this.trumpet4.nickName 
+        && this.trumpet5.nickName);
+      this.trumpet4Players=this.musicians
+      .filter(item => item.axe==='trumpet')
+        .filter(item => item.nickName !== this.trumpet1.nickName 
+        && this.trumpet2.nickName && this.trumpet3.nickName 
+        && this.trumpet5.nickName);
+      this.trumpet5Players=this.musicians
+      .filter(item => item.axe==='trumpet')
+        .filter(item => item.nickName !== this.trumpet1.nickName 
+        && this.trumpet2.nickName && this.trumpet3.nickName 
+        && this.trumpet4.nickName);
       this.alto1Players = this.musicians
         .filter(item => item.axe==='alto')
         .filter(item => item.nickName !== this.alto2.nickName);
       this.alto2Players = this.musicians
         .filter(item => item.axe==='alto')
         .filter(item => item.nickName !== this.alto1.nickName);
-      this.tenorPlayers=this.musicians.filter(item => item.axe==='tenor');
+      this.tenor1Players=this.musicians.filter(item => item.axe==='tenor')
+        .filter(item => item.axe==='tenor')
+        .filter(item => item.nickName !== this.tenor2.nickName);
+      this.tenor2Players=this.musicians.filter(item => item.axe==='tenor')
+        .filter(item => item.axe==='tenor')
+        .filter(item => item.nickName !== this.tenor1.nickName);
       this.bariPlayers=this.musicians.filter(item => item.axe==='bari');
-      this.bonePlayers=this.musicians.filter(item => item.axe==='bone');
+      this.bone1Players=this.musicians.filter(item => item.axe==='bone')
+        .filter(item => item.axe==='bone')
+        .filter(item => item.nickName !== this.bone2.nickName 
+        && this.bone3.nickName && this.bone4.nickName && this.bone5.nickName);
+      this.bone2Players=this.musicians.filter(item => item.axe==='bone')
+        .filter(item => item.axe==='bone')
+        .filter(item => item.nickName !== this.bone1.nickName 
+        && this.bone3.nickName && this.bone4.nickName && this.bone5.nickName);
+      this.bone3Players=this.musicians.filter(item => item.axe==='bone')
+        .filter(item => item.axe==='bone')
+        .filter(item => item.nickName !== this.bone1.nickName 
+        && this.bone2.nickName && this.bone4.nickName && this.bone5.nickName);
+      this.bone4Players=this.musicians.filter(item => item.axe==='bone')
+        .filter(item => item.axe==='bone')
+        .filter(item => item.nickName !== this.bone1.nickName 
+        && this.bone2.nickName && this.bone3.nickName && this.bone5.nickName);
+      this.bone5Players=this.musicians.filter(item => item.axe==='bone')
+        .filter(item => item.axe==='bone')
+        .filter(item => item.nickName !== this.bone1.nickName 
+        && this.bone2.nickName && this.bone3.nickName && this.bone4.nickName);
       this.pianoPlayers=this.musicians.filter(item => item.axe==='piano');
       this.bassPlayers=this.musicians.filter(item => item.axe==='bass');
       this.drummerPlayers=this.musicians.filter(item => item.axe==='drummer');
